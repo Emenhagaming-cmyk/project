879baa6dfcdf5dcaef94de804f4f9a1a24a1db3d
Emenhagaming-cmyk <zakkyilhamfirmansyahhh@gmail.com>
2026-01-14 05:20:33 +0000

    new

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('halo aku zakky');
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>Server dari HP 🚀</h1>
+    <p>Node.js + Express jalan di Ubuntu UserLAnd</p>
+    <p>Repo: GitHub tersambung ✔️</p>
+  `);
+});
+
+app.listen(3000, () => {
+  console.log("Server jalan di http://localhost:3000");
+});const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Web server dari hp hhh 🚀</h1><p>MyApp sudah online</p>");
+});
+
+app.listen(3000, () => {
+  console.log("Server jalan di http://localhost:3000");
+});
